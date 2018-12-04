bb0da45a36ce704177d7d37d102eef926d9191a9
onurcemkarakoc <onurcemkobala@gmail.com>
2018-12-04 16:31:13 +0300

    deneme

--- src/main/java/com/elibrary/elibrary/graphql/Mutation.java
+++ src/main/java/com/elibrary/elibrary/graphql/Mutation.java
@@ -2,16 +2,34 @@ package com.elibrary.elibrary.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.elibrary.elibrary.domain.Author;
+import com.elibrary.elibrary.domain.User;
 import com.elibrary.elibrary.repository.AuthorRepository;
+import com.elibrary.elibrary.repository.UserRepository;
+
+import java.util.Optional;
 
 public class Mutation implements GraphQLMutationResolver {
     private AuthorRepository authorRepository;
+    private UserRepository userRepository;
 
-    public Mutation(AuthorRepository authorRepository) {
+    public Mutation(AuthorRepository authorRepository,UserRepository userRepository) {
+        this.userRepository = userRepository;
         this.authorRepository = authorRepository;
     }
 
     public Author createAuthor(String firstName, String lastName) {
         return authorRepository.save(new Author(firstName, lastName));
     }
+
+    public User transformUser(long id) {
+        User user=userRepository.getOne(id);
+        if(user.isRequested()){
+            user.setRequested(false);
+        }else{
+            user.setRequested(true);
+        }
+        return userRepository.save(user);
+    }
+
+
 }
--- src/main/java/com/elibrary/elibrary/graphql/Query.java
+++ src/main/java/com/elibrary/elibrary/graphql/Query.java
@@ -2,16 +2,22 @@ package com.elibrary.elibrary.graphql;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.elibrary.elibrary.domain.Author;
+import com.elibrary.elibrary.domain.User;
 import com.elibrary.elibrary.repository.AuthorRepository;
+import com.elibrary.elibrary.repository.UserRepository;
 
 public class Query implements GraphQLQueryResolver {
     private AuthorRepository authorRepository;
-
-    public Query(AuthorRepository authorRepository) {
+    private UserRepository userRepository;
+    public Query(AuthorRepository authorRepository,UserRepository userRepository) {
         this.authorRepository = authorRepository;
+        this.userRepository = userRepository;
     }
 
     public Iterable<Author> authors() {
         return authorRepository.findAll();
+    }
+    public Iterable<User> users() {
+        return userRepository.findAll();
     }
 }
