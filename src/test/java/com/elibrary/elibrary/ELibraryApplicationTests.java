35b1d0a3d50da1a8f2d7f77806fc928053d5fc41
onurcemkarakoc <onurcemkobala@gmail.com>
2018-12-09 18:08:42 +0300

    BookCategoryRepository_test

--- src/test/java/com/elibrary/elibrary/ELibraryApplicationTests.java
+++ src/test/java/com/elibrary/elibrary/ELibraryApplicationTests.java
@@ -11,6 +11,7 @@ public class ELibraryApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
---
+++ src/test/java/com/elibrary/elibrary/repository/BookCategoryTest.java
@@ -0,0 +1,66 @@
+package com.elibrary.elibrary.repository;
+
+import com.elibrary.elibrary.domain.BookCategory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class BookCategoryTest {
+
+    @Autowired
+    private BookCategoryRepository bookCategoryRepository;
+
+    @Test
+    public void testCreateBookCategory(){
+
+        BookCategory bookCategory=new BookCategory("Macera");
+        bookCategoryRepository.save(bookCategory);
+        assertNotEquals(0,bookCategory.getId());
+    }
+
+    @Test
+    public void testReadBookCategoryById(){
+        BookCategory bookCategory=new BookCategory("Macera");
+        bookCategoryRepository.save(bookCategory);
+        BookCategory _bookCategory = bookCategoryRepository.getOne(bookCategory.getId());
+        assertNotNull(_bookCategory);
+        assertEquals(_bookCategory.getName(),bookCategory.getName());
+    }
+
+    @Test
+    public void testUpdateBookCategoryById(){
+        BookCategory bookCategory=new BookCategory("Macera");
+        bookCategoryRepository.save(bookCategory);
+        String _name="Aksiyon";
+        bookCategory.setName(_name);
+        bookCategoryRepository.save(bookCategory);
+        assertEquals(_name,bookCategory.getName());
+    }
+
+    @Test(expected = JpaObjectRetrievalFailureException.class)
+    public  void testDeleteBookCategory(){
+        BookCategory bookCategory=new BookCategory("Macera");
+        bookCategoryRepository.save(bookCategory);
+        long id = bookCategory.getId();
+        bookCategoryRepository.delete(bookCategory);
+        bookCategoryRepository.getOne(id); // error
+
+    }
+
+
+
+}
