e9c3d623e920cc2b807b8b261f2f3cba8b2fd1dc
Onur Yartaşı <onuryartasi@live.com>
2018-12-11 00:49:25 +0300

    adding post id request

--- src/main/java/com/elibrary/elibrary/controller/BookController.java
+++ src/main/java/com/elibrary/elibrary/controller/BookController.java
@@ -14,18 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.elibrary.elibrary.domain.Book;
+import com.elibrary.elibrary.repository.BookRepository;
+
 @RestController
 @RequestMapping("/file")
 public class BookController {
     private static String UPLOADED_FOLDER = "books//";
+    private BookRepository bookRepository;
 	
     @GetMapping("/")
     public String index() {
         return "upload";
     }
+    public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
     
     @PostMapping("/upload") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+    public String singleFileUpload(@RequestParam("id") long id, @RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
         if (file.isEmpty()) {
@@ -39,7 +46,9 @@ public class BookController {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
-
+            Book book = bookRepository.findById(id).get();
+            book.setFilePath(String.valueOf(path));
+            
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
 
