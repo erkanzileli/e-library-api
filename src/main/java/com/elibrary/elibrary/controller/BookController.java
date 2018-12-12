524b78db54fdb0dde6991f473e01f8c70671c208
Onur Yartaşı <onuryartasi@live.com>
2018-12-12 22:15:17 +0300

    book download

--- src/main/java/com/elibrary/elibrary/controller/BookController.java
+++ src/main/java/com/elibrary/elibrary/controller/BookController.java
@@ -1,21 +1,35 @@
 package com.elibrary.elibrary.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.elibrary.elibrary.domain.Book;
 import com.elibrary.elibrary.repository.BookRepository;
+import com.mysql.cj.log.Log;
 
 @RestController
 @RequestMapping("/file")
@@ -32,7 +46,7 @@ public class BookController {
 	}
     
     @PostMapping("/upload") // //new annotation since 4.3
-    public String singleFileUpload(@RequestParam("id") long id, @RequestParam("file") MultipartFile file,
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
         if (file.isEmpty()) {
@@ -46,8 +60,8 @@ public class BookController {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
             Files.write(path, bytes);
-            Book book = bookRepository.findById(id).get();
-            book.setFilePath(String.valueOf(path));
+            //Book book = bookRepository.findById(id).get();
+            //book.setFilePath(String.valueOf(path));
             
             redirectAttributes.addFlashAttribute("message",
                     "You successfully uploaded '" + file.getOriginalFilename() + "'");
@@ -63,5 +77,28 @@ public class BookController {
     public String uploadStatus() {
         return "uploadStatus";
     }
+    @RequestMapping("/download/{fileName:.+}")
+    public void downloadPDFResource( HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     @PathVariable("fileName") String fileName)
+    {
+    	
+        String dataDirectory = "books";
+        Path file = Paths.get(dataDirectory, fileName);
+        if (Files.exists(file))
+        {
+        	response.setContentType("application/pdf");
+            response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+            try
+            {
+                Files.copy(file, response.getOutputStream());
+                response.getOutputStream().flush();
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
 
 }
